31e99c3d22949640ec1f6716fc38ca08519c56f3
Parker Thomas <pthomas1997@gmail.com>
2015-09-25 20:24:04 -0400

    general updates, switch iframes to webviews. Canvas working!

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var app = require('app');
+var Tray = require('tray');
+var Menu = require('menu');
 var BrowserWindow = require('browser-window');
 
 require('crash-reporter').start();
@@ -12,8 +14,15 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 730});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  var appIcon = new Tray('assets/favicon-16x16.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Home', type: 'normal'  }
+  ]);
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
 
   //mainWindow.openDevTools();
 
